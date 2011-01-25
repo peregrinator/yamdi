88326523e416595a600d1abce5f4da0b95ed9bb4
Bob Burbach <bob.burbach@gmail.com>
2011-01-25 12:22:28 -0800

    Add documentation for paperclip post processor and ensuring the attachment instance is saved when using background paperclip processing.

--- lib/paperclip_processors/metadata_extractor.rb
+++ lib/paperclip_processors/metadata_extractor.rb
@@ -7,7 +7,15 @@ module Paperclip
       # add values to the attachment instance (for db persistence, etc)
       # assumes duration is a column in the table this attachment is being
       # added to.
+      
+      # a simple assignment works if you are not background processing your 
+      # paperclip processing
       attachment.instance.duration = metadata.duration
+
+      # if you are background processing your paperclip processing which is 
+      # common when working with videos you'll want to ensure the instance gets
+      # saved at some point in the processor chain, use:
+      # attachment.instance.update_attribute(:duration, metadata.duration)
       
       # always return a reference to the file when done
       file
